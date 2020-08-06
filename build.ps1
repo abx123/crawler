287a672cc2b64c9cac1509949807db96e4cbc592
Wing Ming Sam <wmsam91@gmail.com>
2020-08-06 10:45:06 +0800

    test

--- build.ps1
+++ build.ps1
@@ -3,4 +3,4 @@ $Env:CGO_ENABLED = "0"
 $Env:GOARCH = "amd64"
 go build -o main main.go
 mkdir outputs
-~\Go\Bin\build-lambda-zip.exe -output outputs\main.zip main
+~\Go\Bin\build-lambda-zip.exe -output main.zip main
